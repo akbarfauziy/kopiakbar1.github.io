c3e491124c1a2add28a46685716f06f6b97f8c50
Akbar Fauziy <akbarfauziy332@gmail.com>
2024-01-02 21:48:14 +0700

    1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toogle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const menu = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e) {
+    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
